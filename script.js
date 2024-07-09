80040c556f50e4c8d2fd60cd0465dfabcf60af23
arbab57 <165644659+arbab57@users.noreply.github.com>
2024-07-10 01:13:09 +0500

    changd according to test

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#text-input");
 const result = document.querySelector("#result");
-const btnCheck = document.querySelector("#button-check");
+const btnCheck = document.querySelector("#check-btn");
 let word = "";
+
+const abc = "/[a-z]/gi";
 
 btnCheck.onclick = (e) => {
   e.preventDefault();
   if (input.value === "") {
     result.textContent = "Please enter a value";
+    window.alert("Please input a value");
     return;
   }
-  word = input.value;
+  word = input.value
+    .match(/[a-z0-9]/gi)
+    .join("")
+    .toLowerCase();
   checkIfPalindrome(word);
   input.value = "";
 };
@@ -18,8 +24,8 @@ btnCheck.onclick = (e) => {
 function checkIfPalindrome(wrd) {
   let reverseWord = wrd.split("").reverse().join("");
   if (reverseWord !== wrd) {
-    result.innerHTML = `<span class="fw-bold">${wrd}</span> is not a <i>Palindrome.</i>`;
+    result.innerHTML = `<span class="fw-bold">${input.value}</span> is not a <i>palindrome.</i>`;
     return;
   }
-  result.innerHTML = `<span class="fw-bold">${wrd}</span> is a <i>Palindrome.</i>`;
+  result.innerHTML = `<span class="fw-bold">${input.value}</span> is a <i>palindrome.</i>`;
 }
